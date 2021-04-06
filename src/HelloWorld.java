aa8a072031fa8c98cad11e550234de65a4751871
Cory Lobb <cory.lobb@acc.co.nz>
2021-04-07 11:24:54 +1200

    initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("HelloWorld1");
-        System.out.println ("Master Three way Merge");
+        System.out.println ("Master Three Updated Gitpush");
     }
 }
