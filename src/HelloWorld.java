7a4ea883a51ff9c0739924cc5dc7c6ab5db7a1d8
Cory Lobb <cory.lobb@acc.co.nz>
2021-04-07 11:26:43 +1200

    initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("HelloWorld1");
-        System.out.println ("Master Three Updated Gitpush");
+        System.out.println ("Master Cory Updated another Gitpush");
     }
 }
